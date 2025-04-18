f2bf136b5c48d03e4818582fdbd9fb5e926f7131
Nilson <pumanelca@gmail.com>
2025-04-17 21:00:29 -0500

    create component login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const {email, password} = this.loginForm.value;
+      console.log('Login data:', email, password);
+      // Aquí va la lógica de autenticación real
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onRegister() {
+    console.log('Redirigir al registro');
+  }
+
+}
